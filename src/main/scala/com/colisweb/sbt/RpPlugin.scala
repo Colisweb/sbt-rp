04666de34b5e5ff8d64b54d1d6c613b5715b524d
jules Ivanic <jules.ivanic@gmail.com>
2018-06-21 10:51:36 +0200

    Add yaml files generated to the list of files to clean

--- src/main/scala/com/colisweb/sbt/RpPlugin.scala
+++ src/main/scala/com/colisweb/sbt/RpPlugin.scala
@@ -10,7 +10,8 @@ import scala.sys.process.Process
 object RpPlugin extends AutoPlugin {
 
   object autoImport {
-    lazy val yamlsDir: TaskKey[File]                               = taskKey[File]("TODO")
+    lazy val yamlDir: TaskKey[File]                                = taskKey[File]("TODO")
+    lazy val yamlFile: TaskKey[File]                               = taskKey[File]("TODO")
     lazy val servicesVersion: TaskKey[String]                      = taskKey[String]("TODO")
     lazy val generateServiceResourcesRepo: TaskKey[String]         = taskKey[String]("TODO")
     lazy val generateServiceResourcesOptions: TaskKey[Seq[String]] = taskKey[Seq[String]]("TODO")
@@ -34,18 +35,21 @@ object RpPlugin extends AutoPlugin {
       "--pod-controller-replicas 3",
       "--pod-controller-image-pull-policy Always"
     ),
+    yamlFile := file(s"${yamlDir.value}/${name.value}.yaml"),
     generateServiceResources := {
       val repo = s"${generateServiceResourcesRepo.value}/${name.value}:${servicesVersion.value}"
       val cmd  = s"rp generate-kubernetes-resources $repo ${generateServiceResourcesOptions.value.mkString(" ")}"
 
       val log: Logger = streams.value.log
-      log.info(s"cmd: $cmd")
 
-      Process(cmd) #> file(s"${yamlsDir.value}/${name.value}.yaml") run log
+      log.info(s"RpPlugin - Executing: $cmd > ${yamlFile.value}")
+
+      Process(cmd) #> yamlFile.value run log
     },
     dist := (dist dependsOn generateServiceResources).value,
     (publish in Docker) := ((publish in Docker) dependsOn generateServiceResources).value,
     (publishLocal in Docker) := ((publishLocal in Docker) dependsOn generateServiceResources).value,
+    cleanFiles += yamlFile.value
   )
 
 }
